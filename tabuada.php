0ab9cb223a786375230ad73a207eed4f78c0f516
Andre Brito <andrebritosantos002@gmail.com>
2023-10-21 00:06:06 -0300

    Desafio: Calcular a tabuada de um número

---
+++ tabuada.php
@@ -0,0 +1,9 @@
+<?php
+
+$numero = 6;
+
+for($i = 1; $i <=10; $i++){
+    $resultado = $numero * $i;
+    // echo "$numero X $i = $resultado". PHP_EOL;
+    echo "$numero X $i = ". $numero*$i. PHP_EOL;
+}
